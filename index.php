b8cd0cd91e3dfaf34d611724625ef2f2eccfcc01
Zohar Babin <zohar.babin@kaltura.com>
2012-06-12 19:35:26 -0400

    pretty things

--- index.php
+++ index.php
@@ -10,28 +10,7 @@
 	</style>
 	<link rel="stylesheet" href="js/prettycheckboxes/css/prettyCheckboxes.css" type="text/css" media="screen" title="prettyComment main stylesheet" charset="utf-8" />
 	<link href="js/loadmask/jquery.loadmask.css" rel="stylesheet" type="text/css" />
-	<!-- Page Style -->
-	<style type="text/css">
-		.notep {
-			font-size: 14px; background: #F2F4D5 url(http://cdnknowledge.kaltura.com//sites/all/themes/kaltura_theme/tinymce_styles/images/note_icons.png) no-repeat left center; padding-left: 80px; min-height: 56px; display: block; padding-top: 5px; margin-top: 15px; margin-bottom: 15px;
-		}
-		.type {
-			padding: 5px 30px 5px 0;
-		}
-		.type-1 {
-			background: transparent url(icons/icon_film.gif) no-repeat center right;
-		}
-		.type-2 {
-			background: transparent url(icons/icon_pic.gif) no-repeat center right;
-		}
-		.type-5 {
-			background: transparent url(icons/icon_music.gif) no-repeat center right;
-		}
-		.downloadlink {
-			padding: 5px 42px 5px 0;
-			background: transparent url(icons/icon_download.png) no-repeat center right;
-		}
-	</style>
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
 	<!-- Script Includes -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript" language="javascript" src="js/datatables/media/js/jquery.dataTables.min.js"></script>
@@ -60,10 +39,8 @@
 				},
 				"fnServerData": function ( sSource, aoData, fnCallback ) {
 					$.getJSON( sSource, aoData, function (json) { 
-						/* Do whatever additional processing you want on the callback, then tell DataTables */
-						$("#sourcecode").html(json.codesample);
-						//console.log(json.codesample);
-						fnCallback(json);
+						$("#sourcecode").html(json.codesample); //print the source code to the page before printing the table
+						fnCallback(json); //finalize dataTables run
 					} );
 				}
 			} );
@@ -103,54 +80,61 @@
 <body>
 <a href="https://github.com/kaltura/KalturaAPISampleListEntries" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
 <article style="display:block;width:90%;margin: 0px auto;">
-	<h1>Listing Kaltura <span style="font-style:italic;">Media</span> Entries...</h1>
-	<p>This sample shows how to use jQuery datatables and Kaltura's PHP API Client Library to create a searchable and sortable list of Kaltura Media Entries.
+	<h1>Listing Kaltura Media Entries...</h1>
+	<p>This sample shows how to use Kaltura's PHP API Client Library to create a searchable and sortable list of Kaltura Media Entries.
 	<br />The <a href="#sourcecodeexample">source code example below</a> the table, will automatically be updated according to the filters and sort conditions you set.</p>
 	<p class="notep">NOTE: Make sure to set your partner id and admin secret in getlist.php</p>
-<div id="tableandfilters">
-	<div id="boxes" style="width:20%;float:left;display:block;">
-		<a href="#" onClick="toggleCheckboxes('#status_boxes');"><span class="openindicator">+</span> Filter by Kaltura Entry Status:</a>
-		<div id="status_boxes" style="display:none;">
-			<label for="chk-13" style="text-decoration: underline;">Un/Check All</label>
-			<input type="checkbox" id="chk-13" value="all" onclick="checkAllPrettyCheckboxes(this, $('#status_boxes'));" />
-			<label for="chk-1">Blocked</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-1" value="6"  />
-			<label for="chk-2">Deleted</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-2" value="3"  />
-			<label for="chk-3">Error Converting</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-3" value="-1"  />
-			<label for="chk-4">Error Importing</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-4" value="-2"  />
-			<label for="chk-5">Importing</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-5" value="0"  />
-			<label for="chk-6">In Moderation</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-6" value="5"  />
-			<label for="chk-7">Entry Without Content</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-7" value="7"  />
-			<label for="chk-8">Pending</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-8" value="4"  />
-			<label for="chk-9">Waiting Conversion</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-9" value="1"  />
-			<label for="chk-10">Ready To Play</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-10" value="2"  />
-			<label for="chk-11">Virus Scan Failed</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-11" value="virusScan.ScanFailure"  />
-			<label for="chk-12">Infected with Virus</label>
-			<input type="checkbox" name="filterstatus[braket]" id="chk-12" value="virusScan.Infected"  />
+	<div id="tableandfilters">
+		<div id="boxes" style="width:20%;float:left;display:block;">
+			<div class="accordian">
+				<ul>
+					<li>Filter by Media Type</li>
+					<li>
+						<div id="media_boxes" class="chknoxwrapper">
+							<label for="chk-14">Video</label>
+							<input type="checkbox" name="filtermediatype[braket]" id="chk-14" value="1"  />
+							<label for="chk-15">Audio</label>
+							<input type="checkbox" name="filtermediatype[braket]" id="chk-15" value="5"  />
+							<label for="chk-16">Image</label>
+							<input type="checkbox" name="filtermediatype[braket]" id="chk-16" value="2"  />
+							<label for="chk-all">*Un/Check All</label>
+							<input type="checkbox" id="chk-all" value="all" onclick="checkAllPrettyCheckboxes(this, $('#media_boxes'));" />
+						</div>
+					</li>
+					<li>Filter by Kaltura Entry Status:</li>
+					<li>
+						<div id="status_boxes" class="chknoxwrapper">
+							<label for="chk-1">Blocked</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-1" value="6"  />
+							<label for="chk-2">Deleted</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-2" value="3"  />
+							<label for="chk-3">Error Converting</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-3" value="-1"  />
+							<label for="chk-4">Error Importing</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-4" value="-2"  />
+							<label for="chk-5">Importing</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-5" value="0"  />
+							<label for="chk-6">In Moderation</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-6" value="5"  />
+							<label for="chk-7">Entry Without Content</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-7" value="7"  />
+							<label for="chk-8">Pending</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-8" value="4"  />
+							<label for="chk-9">Waiting Conversion</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-9" value="1"  />
+							<label for="chk-10">Ready To Play</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-10" value="2"  />
+							<label for="chk-11">Virus Scan Failed</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-11" value="virusScan.ScanFailure"  />
+							<label for="chk-12">Infected with Virus</label>
+							<input type="checkbox" name="filterstatus[braket]" id="chk-12" value="virusScan.Infected"  />
+							<label for="chk-13">*Un/Check All</label>
+							<input type="checkbox" id="chk-13" value="all" onclick="checkAllPrettyCheckboxes(this, $('#status_boxes'));" />
+						</div>
+					</li>
+				</ul>
+			</div>
 		</div>
-		<div style="clear:both;"></div>
-		<a href="#" onClick="toggleCheckboxes('#media_boxes');"><span class="openindicator">-</span> Filter by Media Type</a>
-		<div id="media_boxes">
-			<label for="chk-all" style="text-decoration: underline;">Un/Check All</label>
-			<input type="checkbox" id="chk-all" value="all" onclick="checkAllPrettyCheckboxes(this, $('#media_boxes'));" />
-			<label for="chk-14">Video</label>
-			<input type="checkbox" name="filtermediatype[braket]" id="chk-14" value="1"  />
-			<label for="chk-15">Audio</label>
-			<input type="checkbox" name="filtermediatype[braket]" id="chk-15" value="5"  />
-			<label for="chk-16">Image</label>
-			<input type="checkbox" name="filtermediatype[braket]" id="chk-16" value="2"  />
-		</div>
-	</div>
 		<div style="width:80%;float:left;">
 			<table cellpadding="0" cellspacing="0" border="0" class="dataTable" id="dataTable">
 				<thead>
@@ -200,8 +184,10 @@
 			<li>jQuery plugin to mask DOM elements during loading - <a href="http://datatables.net" target="_blank">http://code.google.com/p/jquery-loadmask/</a></li>
 			<li>Icons - <a href="http://pooliestudios.com/projects/iconize/" target="_blank">http://pooliestudios.com/projects/iconize/</a></li>
 			<li>prettyCheckboxes - <a href="http://www.no-margin-for-errors.com/projects/prettycheckboxes/" target="_blank">http://www.no-margin-for-errors.com/projects/prettycheckboxes/</a></li>
+			<li>JQuery Accordion Menu - <a href="http://www.lateralcode.com/jquery-accordion-menu/" target="_blank">http://www.lateralcode.com/jquery-accordion-menu/</a></li>
 		</ul>
 	</div>
 </article>
+<script type="text/javascript" src="js/accordion-menu/jMenu.js"></script>
 </body>
 </html>
